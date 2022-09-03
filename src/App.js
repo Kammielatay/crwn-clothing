fc7694b3422d2a7dec365aadfcc86b0d7242607c
kammielatay <kameshialatay@gmail.com>
2022-09-03 16:57:50 -0700

    updating directory page and category component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './categories.styles.scss';
+//import './categories.styles.scss';
+import Directory from './components/directory/directory.components';
 
 const App = () => {
   const categories = [
@@ -31,17 +32,7 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-          }} />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
+      <Directory categories={categories}/>
     </div>
   );
 }
